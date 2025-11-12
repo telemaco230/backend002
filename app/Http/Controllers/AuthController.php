e1b3c69fae1941a962c48b979820fe49d16ec275
Lorenzo Ramírez <telemaco230@gmail.com>
2025-11-12 21:54:04 +0100

    API, autenticación: revisión
    Revisión de los puntos de API
    Creación de la colección de operaciones para la plataforma Postman.  Ver directorio `/docs`.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Log;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
 
@@ -13,6 +14,9 @@ class AuthController extends Controller
 {
     public function register(Request $request)
     {
+        $prefix = __CLASS__ . '::' . __FUNCTION__ . ' -> ';
+        Log::debug($prefix . " start");
+        Log::debug($prefix . " request:\n" . json_encode($request->all(), JSON_PRETTY_PRINT));
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
@@ -28,6 +32,8 @@ class AuthController extends Controller
         try {
             $token = JWTAuth::fromUser($user);
         } catch (JWTException $e) {
+            Log::error($prefix . " exception:\n" . $e->getMessage() . VarDumper::dumpAsString($request->all()));
+            Log::error($prefix . " stop");
             return response()->json(['error' => 'Could not create token'], 500);
         }
 
@@ -39,6 +45,10 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
+        $prefix = __CLASS__ . '::' . __FUNCTION__ . ' -> ';
+        Log::debug($prefix . " start");
+        Log::debug($prefix . " request:\n" . json_encode($request->all(), JSON_PRETTY_PRINT));
+
         $credentials = $request->only('email', 'password');
 
         try {
@@ -46,6 +56,8 @@ class AuthController extends Controller
                 return response()->json(['error' => 'Invalid credentials'], 401);
             }
         } catch (JWTException $e) {
+            Log::error($prefix . " exception:\n" . $e->getMessage());
+            Log::error($prefix . " stop");
             return response()->json(['error' => 'Could not create token'], 500);
         }
 
@@ -57,9 +69,13 @@ class AuthController extends Controller
 
     public function logout()
     {
+        $prefix = __CLASS__ . '::' . __FUNCTION__ . ' -> ';
+        Log::debug($prefix . " start");
         try {
             JWTAuth::invalidate(JWTAuth::getToken());
         } catch (JWTException $e) {
+            Log::error($prefix . " exception:\n" . $e->getMessage());
+            Log::error($prefix . " stop");
             return response()->json(['error' => 'Failed to logout, please try again'], 500);
         }
 
@@ -68,6 +84,8 @@ class AuthController extends Controller
 
     public function getUser()
     {
+        $prefix = __CLASS__ . '::' . __FUNCTION__ . ' -> ';
+        Log::debug($prefix . " start");
         try {
             $user = Auth::user();
             if (!$user) {
@@ -75,17 +93,27 @@ class AuthController extends Controller
             }
             return response()->json($user);
         } catch (JWTException $e) {
+        Log::debug($prefix . " stop");
+            Log::error($prefix . " exception:\n" . $e->getMessage());
+            Log::error($prefix . " stop");
             return response()->json(['error' => 'Failed to fetch user profile'], 500);
         }
     }
 
     public function updateUser(Request $request)
     {
+        $prefix = __CLASS__ . '::' . __FUNCTION__ . ' -> ';
+        Log::debug($prefix . " start");
+        Log::debug($prefix . " request:\n" . json_encode($request->all(), JSON_PRETTY_PRINT));
         try {
             $user = Auth::user();
             $user->update($request->only(['name', 'email']));
+            Log::debug($prefix . " User updated succe");
+            Log::debug($prefix . " stop");
             return response()->json($user);
         } catch (JWTException $e) {
+            Log::error($prefix . " exception:\n" . $e->getMessage() . VarDumper::dumpAsString($request->all()));
+            Log::error($prefix . " stop");
             return response()->json(['error' => 'Failed to update user'], 500);
         }
     }
